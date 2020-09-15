32a9e54be7e66c64fd8a5027b6d0f4966aaaf0e5
Michael Liu <michael.liu0@gmail.com>
2020-09-15 12:12:36 -0700

    Fix bad index

--- src/DBConnector.js
+++ src/DBConnector.js
@@ -4,5 +4,5 @@ export const client = new fauna.Client({ secret: process.env.FAUNA_KEY })
 export const q = fauna.query
 
 export const getUserByEmail = async email => {
-    return await client.query(q.Get(q.Match(q.Index('users_by_email'), email)))
+    return await client.query(q.Get(q.Match(q.Index('user_by_email'), email)))
 }
